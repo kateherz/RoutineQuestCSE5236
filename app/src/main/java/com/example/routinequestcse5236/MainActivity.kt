b90c8618f3d3ae0f609864987d1dac2c06792915
Kate Herz <k8herz4@verizon.net>
2024-02-20 20:55:51 -0500

    add enter name test and next button

--- app/src/main/java/com/example/routinequestcse5236/MainActivity.kt
+++ app/src/main/java/com/example/routinequestcse5236/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.routinequestcse5236
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var nextButton: Button
+    //function onCreate called when an instance of the activity subclass is created
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        nextButton = findViewById(R.id.next_button)
+        nextButton.setOnClickListener {view: View ->
+            //do something in response to the click here
+            Toast.makeText(
+                this,
+                R.string.input_name_toast,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }
