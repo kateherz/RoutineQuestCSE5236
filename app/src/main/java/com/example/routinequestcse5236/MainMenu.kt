8365ea740e73accf069542d915963684cdf1e62e
azar0420 <azar.17@osu.edu>
2024-03-04 17:02:33 -0500

    create Main Menu activity and XML layout

---
+++ app/src/main/java/com/example/routinequestcse5236/MainMenu.kt
@@ -0,0 +1,10 @@
+package com.example.routinequestcse5236
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainMenu: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
