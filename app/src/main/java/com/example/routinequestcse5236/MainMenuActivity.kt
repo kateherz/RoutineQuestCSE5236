727572d735b7edda91e0f9cee36bfe3703de3e67
azar0420 <azar.17@osu.edu>
2024-03-05 18:21:10 -0500

    connect main menu activity to the avatar confirmation on click

--- app/src/main/java/com/example/routinequestcse5236/MainMenuActivity.kt
+++ app/src/main/java/com/example/routinequestcse5236/MainMenuActivity.kt
@@ -6,5 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 class MainMenuActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_menu)
     }
 }
