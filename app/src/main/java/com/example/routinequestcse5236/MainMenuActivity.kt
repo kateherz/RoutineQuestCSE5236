fd9dca3b918b702ee078fdf3710a0e284ed6f6f8
azar0420 <azar.17@osu.edu>
2024-03-05 18:13:09 -0500

    create rough transition from avatar creation to main menu

--- app/src/main/java/com/example/routinequestcse5236/MainMenuActivity.kt
+++ app/src/main/java/com/example/routinequestcse5236/MainMenuActivity.kt
@@ -3,7 +3,7 @@ package com.example.routinequestcse5236
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainMenu: AppCompatActivity() {
+class MainMenuActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
