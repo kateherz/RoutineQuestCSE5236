96008054981779bdc4dca012b47c329d8f05f464
Kate Herz <k8herz4@verizon.net>
2024-03-06 20:45:45 -0500

    Update gradle files

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,21 @@ plugins {
     id("com.android.application") version "8.2.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
+
+/*buildscript{
+    ext.kotlin_verison = "1.5.31"
+}*/
+ext {
+    var activityVersion = "1.4.0"
+    var appCompatVersion = "1.4.0"
+    var constraintLayoutVersion = "2.1.2"
+    var coreTestingVersion = "2.1.0"
+    var coroutines = "1.5.2"
+    var lifecycleVersion = "2.4.0"
+    var materialVersion = "1.4.0"
+    var roomVersion = "2.3.0"
+    // testing
+    var junitVersion = "4.13.2"
+    var espressoVersion = "3.4.0"
+    var androidxJunitVersion = "1.1.3"
+}
