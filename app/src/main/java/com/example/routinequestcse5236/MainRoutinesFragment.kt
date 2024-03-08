0f0e72166013b0f20ff91ca7081561738e516e62
lnawani96 <lisha.nawani@gmail.com>
2024-03-08 18:17:08 -0500

    crud functions done!!

--- app/src/main/java/com/example/routinequestcse5236/MainRoutinesFragment.kt
+++ app/src/main/java/com/example/routinequestcse5236/MainRoutinesFragment.kt
@@ -8,12 +8,22 @@ import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FieldPath
+import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.SetOptions
+import com.google.firebase.firestore.firestore
 
 class MainRoutinesFragment : Fragment() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var routineAdapter: RoutineAdapter
     private lateinit var addButton: Button
+    private lateinit var deleteButton: Button
     private lateinit var routines : ArrayList<Routine>
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var databaseRef: FirebaseFirestore
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,14 +33,57 @@ class MainRoutinesFragment : Fragment() {
         Log.d("MainRoutinesFragment", "OnCreateView called")
         var v: View = inflater.inflate(R.layout.fragment_routines, container, false)
 
+
+        databaseRef = Firebase.firestore
+        firebaseAuth = FirebaseAuth.getInstance()
         routines = ArrayList()
 
-        val addButton= v.findViewById<Button>(R.id.addMoreTasks)
+        addButton = v.findViewById<Button>(R.id.addMoreTasks)
         addButton.setOnClickListener {
             Log.d("addButton", "Button Pressed")
             routines.add(Routine("task",9))
             routineAdapter.notifyDataSetChanged()
             Log.d("addButton", routineAdapter.itemCount.toString())
+            val data = hashMapOf("routines" to routines)
+            databaseRef
+                .collection("users")
+                .document(firebaseAuth.currentUser?.email.toString())
+                .set(data, SetOptions.merge())
+                .addOnSuccessListener {
+                    Log.d("Firebase", "routines added to user successfully")
+                }
+        }
+
+        deleteButton = v.findViewById(R.id.deleteTask)
+        deleteButton.setOnClickListener {
+            Log.d("deleteButton", "Button Pressed")
+            if (routines.size >= 1) {
+                routines.removeLast()
+                routineAdapter.notifyDataSetChanged()
+
+                if (routines.size == 0) {
+                    val updates = hashMapOf<String, Any>(
+                        "routines" to FieldValue.delete(),
+                    )
+                    databaseRef
+                        .collection("users")
+                        .document(firebaseAuth.currentUser?.email.toString())
+                        .update(updates)
+                        .addOnSuccessListener {
+                            Log.d("Firebase", "routines field deleted successfully")
+                        }
+                }
+                else {
+                    val data = hashMapOf("routines" to routines)
+                    databaseRef
+                        .collection("users")
+                        .document(firebaseAuth.currentUser?.email.toString())
+                        .set(data, SetOptions.merge())
+                        .addOnSuccessListener {
+                            Log.d("Firebase", "last routine deleted successfully")
+                        }
+                }
+            }
         }
 
         return v
--- app/src/main/java/com/example/routinequestcse5236/SignupFragment.kt
+++ app/src/main/java/com/example/routinequestcse5236/SignupFragment.kt
@@ -10,7 +10,10 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import androidx.fragment.app.Fragment
+import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.firestore
 
 
 class SignupFragment : Fragment() {
@@ -18,6 +21,7 @@ class SignupFragment : Fragment() {
     private var mEmailEditText: EditText? = null
     private var mPasswordEditText: EditText? = null
     private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var databaseRef: FirebaseFirestore
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,11 +29,12 @@ class SignupFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         Log.d("SignupFragment", "onCreateView()")
-        var v: View = inflater.inflate(R.layout.fragment_signup, container, false)
+        val v: View = inflater.inflate(R.layout.fragment_signup, container, false)
 
         mEmailEditText = v.findViewById<EditText>(R.id.email_text)
         mPasswordEditText = v.findViewById<EditText>(R.id.password_text)
         firebaseAuth = FirebaseAuth.getInstance()
+        databaseRef = Firebase.firestore
 
         val loginButton: Button = v.findViewById<Button>(R.id.login_button)
         val signupButton: Button = v.findViewById<Button>(R.id.signup_button)
@@ -54,6 +59,17 @@ class SignupFragment : Fragment() {
                .addOnCompleteListener {
                    if (it.isSuccessful) {
                        Log.d("Firebase", "sign up successful")
+                       val user = hashMapOf(
+                           "email" to mEmailEditText?.text.toString(),
+                           "password" to mPasswordEditText?.text.toString(),
+                       )
+                       databaseRef
+                           .collection("users")
+                           .document(mEmailEditText?.text.toString())
+                           .set(user)
+                           .addOnSuccessListener {
+                               Log.d("Firebase", "user added to firestore")
+                           }
                        val intent = Intent(v.context, AvatarCreationActivity::class.java)
                        startActivity(intent)
                    }
