38924c345642898e3ed346a13f5615184bf34586
lnawani96 <lisha.nawani@gmail.com>
2024-03-08 18:17:08 -0500

    crud functions done!!

--- app/src/main/java/com/example/routinequestcse5236/MainMenuActivity.kt
+++ app/src/main/java/com/example/routinequestcse5236/MainMenuActivity.kt
@@ -4,7 +4,13 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.auth.User
+import com.google.firebase.firestore.firestore
 
 class MainMenuActivity: AppCompatActivity() {
     private lateinit var createButton: Button
