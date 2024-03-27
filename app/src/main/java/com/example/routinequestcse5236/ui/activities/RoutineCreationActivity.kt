756d0d26b8928beeee52636df8cfe2b3fc50f5bf
azar0420 <azar.17@osu.edu>
2024-03-27 19:36:36 -0400

    add button to activity

--- app/src/main/java/com/example/routinequestcse5236/ui/activities/RoutineCreationActivity.kt
+++ app/src/main/java/com/example/routinequestcse5236/ui/activities/RoutineCreationActivity.kt
@@ -14,11 +14,13 @@ class RoutineCreationActivity : AppCompatActivity() {
     private var questName : EditText? = null //persist in firebase
     private var taskList: ArrayList<Task> = arrayListOf() //persist in firebase
     private lateinit var addTaskButton: Button
+    private lateinit var saveButton : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d("RoutineCreationActivity", "OnCreate called")
         setContentView(R.layout.routine_creation)
         questName = findViewById(R.id.routine_name)
         addTaskButton = findViewById(R.id.addMoreTasks)
+        saveButton = findViewById(R.id.saveButton)
     }
 }
