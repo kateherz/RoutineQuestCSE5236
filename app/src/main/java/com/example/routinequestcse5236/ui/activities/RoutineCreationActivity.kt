2c90dbbe96a723a05b397c6ec2bdfbe220a2a46b
azar0420 <azar.17@osu.edu>
2024-03-27 19:29:54 -0400

    Create more UI views for activity and recycler view

--- app/src/main/java/com/example/routinequestcse5236/ui/activities/RoutineCreationActivity.kt
+++ app/src/main/java/com/example/routinequestcse5236/ui/activities/RoutineCreationActivity.kt
@@ -3,16 +3,22 @@ package com.example.routinequestcse5236.ui.activities
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.util.Log
+import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.example.routinequestcse5236.R
+import com.example.routinequestcse5236.model.Task
+import com.google.android.gms.tasks.Tasks
 
 class RoutineCreationActivity : AppCompatActivity() {
     private var questName : EditText? = null //persist in firebase
+    private var taskList: ArrayList<Task> = arrayListOf() //persist in firebase
+    private lateinit var addTaskButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d("RoutineCreationActivity", "OnCreate called")
         setContentView(R.layout.routine_creation)
         questName = findViewById(R.id.routine_name)
+        addTaskButton = findViewById(R.id.addMoreTasks)
     }
 }
