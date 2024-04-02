b2dd6d55c6a8500ff1c7737d027900341527bcf4
lnawani96 <lisha.nawani@gmail.com>
2024-04-02 13:27:13 -0400

    correct avatar and username display on main menu

--- app/src/main/java/com/example/routinequestcse5236/ui/activities/MainMenuActivity.kt
+++ app/src/main/java/com/example/routinequestcse5236/ui/activities/MainMenuActivity.kt
@@ -4,21 +4,50 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
-import android.widget.Toast
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.routinequestcse5236.R
 import com.example.routinequestcse5236.ui.fragments.MainRoutinesFragment
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.firestore
 
 class MainMenuActivity: AppCompatActivity() {
     private lateinit var createButton: Button
+    private lateinit var avatarDisplay: ImageView
+    private lateinit var welcomeMessage: TextView
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var databaseRef: FirebaseFirestore
     //declare levels, xp of user, and total xp needed
     //declare name of user
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d("MainMenuActivity", "onCreate()")
+        databaseRef = Firebase.firestore
+        firebaseAuth = FirebaseAuth.getInstance()
         setContentView(R.layout.activity_main_menu)
+        avatarDisplay = findViewById(R.id.profilePicture)
+        welcomeMessage = findViewById(R.id.welcome)
+
+        val docRef = databaseRef.collection("users").document(firebaseAuth.currentUser?.email.toString())
+        docRef.get()
+            .addOnSuccessListener { document ->
+                if (document != null) {
+                    var chosenAvatar = document.data?.get("avatar") as String
+                    var username = document.data?.get("username") as String
+                    val avatarId = resources.getIdentifier(chosenAvatar, "drawable", packageName)
+                    avatarDisplay.setImageResource(avatarId)
+                    var welcome = "Welcome, " + username + "! Let's tackle today!"
+                    welcomeMessage.setText(welcome)
+                }
+            }
+
         setUpCreateTasks()
         loadTaskFragments()
+
+
     }
 
     private fun setUpCreateTasks() {
