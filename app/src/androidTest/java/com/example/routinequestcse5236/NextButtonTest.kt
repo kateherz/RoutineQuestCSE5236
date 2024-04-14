f98b633a50c8a77b54248f8ffb25a6c990a09c63
azar0420 <azar.17@osu.edu>
2024-04-14 15:33:58 -0400

    create instrumented UI test to check that okay button launches main menu

--- app/src/androidTest/java/com/example/routinequestcse5236/NextButtonTest.kt
+++ app/src/androidTest/java/com/example/routinequestcse5236/NextButtonTest.kt
@@ -1,25 +1,38 @@
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.matcher.IntentMatchers
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import com.example.routinequestcse5236.R
-import com.example.routinequestcse5236.ui.activities.AvatarCreationActivity
+import com.example.routinequestcse5236.ui.activities.MainMenuActivity
+import com.example.routinequestcse5236.ui.activities.AvatarCreationConfirmationActivity
+import org.junit.After
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
-class AvatarCreationActivityTest {
+class AvatarCreationConfirmationActivityTest {
 
     @get:Rule
-    val activityScenarioRule = ActivityScenarioRule(AvatarCreationActivity::class.java)
+    val activityScenarioRule = ActivityScenarioRule(AvatarCreationConfirmationActivity::class.java)
+
+    @Before
+    fun setup() {
+        Intents.init()
+    }
+
+    @After
+    fun tearDown() {
+        Intents.release()
+    }
 
     @Test
-    fun testNextButtonNavigation() {
-        Espresso.onView(ViewMatchers.withId(R.id.next_button))
+    fun testOkayButtonLaunchesMainMenuActivity() {
+        Espresso.onView(ViewMatchers.withId(R.id.okay_button))
             .perform(ViewActions.click())
 
-        // CheckS if AvatarCreationConfirmationActivity was launched
-        Espresso.onView(ViewMatchers.withText("LET'S GO!"))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        Intents.intended(IntentMatchers.hasComponent(MainMenuActivity::class.java.name))
     }
 }
