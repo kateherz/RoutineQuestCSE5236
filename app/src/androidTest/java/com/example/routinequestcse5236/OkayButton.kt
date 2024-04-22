8286cf9c940abd449816f26349e3564ce13a90d8
azar0420 <azar.17@osu.edu>
2024-04-22 12:55:04 -0400

    fix okay button instrumented UI test

--- app/src/androidTest/java/com/example/routinequestcse5236/OkayButton.kt
+++ app/src/androidTest/java/com/example/routinequestcse5236/OkayButton.kt
@@ -1,14 +1,19 @@
 package com.example.routinequestcse5236
 
+import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import com.example.routinequestcse5236.ui.activities.AvatarCreationConfirmationActivity
+import org.junit.Rule
 import org.junit.Test
 
 class AvatarCreationConfirmationActivityTest {
+
+    @get:Rule
     val activityScenarioRule = ActivityScenarioRule(AvatarCreationConfirmationActivity::class.java)
+
     @Test
     fun testOkayButtonNavigation() {
         Espresso.onView(ViewMatchers.withId(R.id.okay_button))
